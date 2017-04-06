6b1e3df3bea66663ce0599cdebe4ab93944700f0
Thadoju vijay <vijaykumar6566@gmail.com>
2017-04-06 18:50:06 +0530

    Namedpositionalretrieve
    it is retrieving records using from com.ecil.Employee where employeeSalary >=:min and employeeSalary<=:max" (namedpositional parameters)

---
+++ DaoFromemp.java
@@ -0,0 +1,37 @@
+package com.ecil;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+public class DaoFromemp 
+{
+
+public static void main(String[] args)
+
+{
+Session session = HibernateUtility.getSession();
+Query query = session.createQuery("from com.ecil.Employee where employeeSalary >=:min and employeeSalary<=:max");
+
+query.setParameter("min", 30000d);
+query.setParameter("max",80000d);
+List list = query.getResultList();
+
+/**
+ * size of the objects inthe entire table is 10,1 row in table  consider as one object
+ */
+
+System.out.println(list.size());
+for (Object object : list) 
+
+{
+	Employee employee=(Employee)object;
+	System.out.println(employee.getEmployeeNumber());
+	System.out.println(employee.getName());
+	System.out.println(employee.getEmployeeSalary());
+	
+}
+session.close();
+}
+}
